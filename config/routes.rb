2ba1ff557186910a0f79b47f069db617e68ac802
Reeta Olivier <reeta_olivier@hotmail.co.uk>
2023-03-14 10:06:22 -0300

    Index and show views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'flats/index'
+  get 'flats/show'
   devise_for :users
   root to: "pages#home"
-  
+  resources :flats, only: [ :index, :show, :new, :create ]
+
+  # resources :bookings, only [ :new, :create ]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
